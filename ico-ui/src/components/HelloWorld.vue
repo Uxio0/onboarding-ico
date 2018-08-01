605522ef0cfe5d7f316df0cc238e2f465d19d880
Uxio Fuentefria <uxio@gnosis.pm>
2018-08-01 14:01:25 +0200

    Get info from contracts

--- ico-ui/src/components/HelloWorld.vue
+++ ico-ui/src/components/HelloWorld.vue
@@ -4,7 +4,9 @@
     <h1>{{ coinbase }}</h1>
     <h2>Balance {{ balance }}</h2>
     <h2>Weth Balance {{ wethBalance }}</h2>
+    <button @click="wrapEth">Wrap Eth</button>
     <h2>SCM Balance {{ scmBalance }}</h2>
+    <h2>Ico Status {{ icoStatus }}</h2>
   </div>
 </template>
 
@@ -26,37 +28,44 @@ export default {
   },
   data() {
     return {
-      web3: null,
       coinbase: null,
       balance: 0,
       scmBalance: 0,
       wethBalance: 0,
+      icoTimestamp: null,
+      web3: () => null,
+      weth: () => null,
+      token: () => null,
+      ico: () => null
     }
   },
   created() {
     this.loadWeb3()
-    setInterval(() => {if (this.getCoinbase() != this.coinbase) this.loadCoinbase()}, 1000);
   },
   methods: {
-    loadWeb3() {
+    async loadWeb3() {
       if (typeof web3 !== 'undefined') {
+        // eslint-disable-next-line
         console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 Fluyd, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
 
         // Use Mist/MetaMask's provider
+        // eslint-disable-next-line
         let w3 = new Web3(web3.currentProvider)
         this.$set(this, 'web3', () => w3)
-        Ico.setProvider(web3.currentProvider)
-        Token.setProvider(web3.currentProvider)
+        Ico.setProvider(w3.currentProvider)
+        Token.setProvider(w3.currentProvider)
+        Weth.setProvider(w3.currentProvider)
 
-        this.loadCoinbase()
-        /*
-      SimpleStorage.deployed()
-        .then((instance) => instance.address)
-        .then((address) => {
-          this.contractAddress = address
-          this.updateCurrentNumber()
+        let [weth, token, ico] = await Promise.all([Weth.deployed(), Token.deployed(), Ico.deployed()])
+        this.$set(this, 'weth', () => weth)
+        this.$set(this, 'token', () => token)
+        this.$set(this, 'ico', () => ico)
+
+        weth.Deposit((error, result) => {
+          console.log(result)
+          this.loadBalances(this.coinbase) 
         })
-         */
+        setInterval(() => {if (this.getCoinbase() != this.coinbase) this.loadCoinbase()}, 1000);
       }
     },
     getCoinbase() {
@@ -66,17 +75,32 @@ export default {
       this.$set(this, 'coinbase', this.getCoinbase())
     },
     loadBalances(address) {
-      if (this.web3 != null && address != null) {
-        let web3 = this.web3()
+      let values = [this.web3(), this.weth(), this.token()]
+      if (values.every(x => x != null) && address != null ) {
+        let [web3, weth, token] = values
         web3.eth.getBalance(address, (error, result) => this.$set(this, 'balance', result.valueOf()))
-      } else {
-        return 0
+        weth.balanceOf.call(address).then(result => this.$set(this, 'wethBalance', result.valueOf()))
+        token.balanceOf.call(address).then(result => this.$set(this, 'scmBalance', result.valueOf()))
       }
     },
+    loadIcoStatus() {
+      let web3 = this.web3()
+      let ico = this.ico()
+      ico.closedOn.call().then(result => this.$set(this, 'icoTimestamp', result.valueOf()))
+    },
+    wrapEth() {
+      let weis = this.web3().toWei(1, 'ether')
+      this.weth().deposit({value: weis, from: this.coinbase})
+    }
   },
   watch: {
     coinbase(address) {
       this.loadBalances(address)
+    }
+  },
+  computed: {
+    icoStatus() {
+      return (this.icoTimestamp == "0")?"Opened":"Closed"
     }
   }
 }
